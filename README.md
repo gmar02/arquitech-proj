b71ceedf55edf402371ba73c34fc665c171b301c
Guilherme Marinho <gmarinho2002@hotmail.com>
2025-03-10 23:27:09 -0400

    [feat] : integra com banco sqlite

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Projeto em grupo (Tópicos Integradores III)
 
 ## Website com Flask, HTML e Bootstrap
-* Inicialize o ambiente virtual com `source ./.venv/bin/activate` na pasta mãe
-* Inicialize o app com `flask run` também na pasta mãe
+* Inicialize o ambiente virtual `source ./.venv/bin/activate`
+* (opcional) Edite o banco com `sqlite3 data.db`
+* Para criar a tabela para ORM: `from app import database; database.create_all()` no flask shell
+* Inicialize o app com `flask run`
 
 ## Integrantes:
 
@@ -16,3 +18,4 @@
 * https://flask.palletsprojects.com/en/stable/quickstart/
 * https://htmlboilerplates.com/
 * https://www.conventionalcommits.org/en/v1.0.0/
+* https://www.sqlalchemy.org/
--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+database = SQLAlchemy(app)
+
+# tabela de membros do grupo, apenas para teste
+class Membro(database.Model):
+    
+    __tablename__ = "membro"
+
+    id = database.Column(database.Integer, primary_key=True)
+    nome = database.Column(database.String(150), unique=True, nullable=False)
+    matricula = database.Column(database.Integer, unique=True, nullable=True)
+    email = database.Column(database.String(200), unique=True, nullable=True)
+
+    def __repr__(self):
+        return f"Membro(id = {self.id}, nome = {self.nome}, matricula = {self.matricula}, email = {self.email}"
+
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+
+    # obtendo todos os membros
+    membros = Membro.query.all()
+
+    return render_template('index.html', membros=membros)
+
